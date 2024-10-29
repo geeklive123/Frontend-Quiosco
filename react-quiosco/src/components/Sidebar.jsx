244fc58c3402d7923200dc47a611be1342d9fa3f
TioJavi <keima13234@hotmail.com>
2024-10-29 17:05:24 -0400

    subiendo vista de productos

--- react-quiosco/src/components/Sidebar.jsx
+++ react-quiosco/src/components/Sidebar.jsx
@@ -13,9 +13,20 @@ export default function Sidebar() {
                 className="mt-10">
                     {categorias.map(categoria=>(
                          <Categoria
+                            key={categoria.id}
                             categoria={categoria}
                         />
                     ))}
+            </div>
+            <div
+                className="my-5 py-5"
+            >
+                    <button
+                        type="button"
+                        className="text-center bg-red-500 w-full p-3 font-bold text-white"
+                    >
+                        Cancelar Orden
+                    </button>
             </div>
         </aside>
     )   
